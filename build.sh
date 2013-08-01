c2a55cc67ea52f6378e9c1677f23c283ca7f3d8e
Vaneyck <vaneycksemeotoki@gmail.com>
2013-08-01 11:50:00 +0300

    Added debug in the build script and enabled foor loop

--- build.sh
+++ build.sh
@@ -3,19 +3,27 @@
 function buildApp {
 	local version=$1
 
+	echo "# $version # :Changing package name on AndroidManifest.xml"
 	sed -i -e s/com.smssync.portal.million/com.smssync.portal.$version/g AndroidManifest.xml
+
+	echo "# $version # Changing the package name on SmsSyncPortal.java"
 	sed -i -e s/com.smssync.portal.million/com.smssync.portal.$version/g src/com/smssync/portal/million/SmsSyncPortal.java
 
+	echo "# $version # Making proper directory"
 	mkdir src/com/smssync/portal/$version
 	mv src/com/smssync/portal/million/SmsSyncPortal.java src/com/smssync/portal/$version/SmsSyncPortal.java
+
+	echo "# $version # Removing old default directory"
+	rm -r src/com/smssync/portal/million
+
+	echo "# $version # Building the APP"
 	ant debug
 }
 
-#for verison in million two three
-#do
-#	buildApp $version
-#	cp bin/SmsSyncPortal-debug.apk ~/Desktop/SmsSyncPortal-$version-debug.apk
-#	git reset --hard && git clean -df
-#dmillion
+for verison in million two three
+do
+	buildApp $version
+	cp bin/SmsSyncPortal-debug.apk ~/Desktop/SmsSyncPortal-$version-debug.apk
+	git reset --hard && git clean -df
+done
 
-buildApp two
