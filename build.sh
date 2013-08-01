e715dde5112dfc9387e9cc52d1226a38084dbec6
Vaneyck <vaneycksemeotoki@gmail.com>
2013-08-01 11:42:35 +0300

    Debig for build script

--- build.sh
+++ build.sh
@@ -3,19 +3,19 @@
 function buildApp {
 	local version=$1
 
-	sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g AndroidManifest.xml
-	sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g src/com/smssync/portal/one/SmsSyncPortal.java
-	sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g gen/com/smssync/portal/one/BuildConfig.java
-	sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g gen/com/smssync/portal/one/R.java
+	sed -i -e s/com.smssync.portal.million/com.smssync.portal.$version/g AndroidManifest.xml
+	sed -i -e s/com.smssync.portal.million/com.smssync.portal.$version/g src/com/smssync/portal/million/SmsSyncPortal.java
 
 	mkdir src/com/smssync/portal/$version
-	mv src/com/smssync/portal/one/SmsSyncPortal.java src/com/smssync/portal/$version/SmsSyncPortal.java
+	mv src/com/smssync/portal/million/SmsSyncPortal.java src/com/smssync/portal/$version/SmsSyncPortal.java
+	ant debug
 }
 
-for verison in one two three
-do
-	buildApp $version
-	ant debug
-	cp bin/SmsSyncPortal-debug.apk ~/Desktop/SmsSyncPortal-$version-debug.apk
-	git reset --hard && git clean -df
-done
+#for verison in million two three
+#do
+#	buildApp $version
+#	cp bin/SmsSyncPortal-debug.apk ~/Desktop/SmsSyncPortal-$version-debug.apk
+#	git reset --hard && git clean -df
+#dmillion
+
+buildApp two
