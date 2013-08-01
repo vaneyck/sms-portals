7993dfc394a68a69a0868eafd69e2f9693459c7e
Vaneyck <vaneycksemeotoki@gmail.com>
2013-08-01 10:44:16 +0300

    Added build.sh

---
+++ sms-portal-1/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$version=$1
+
+sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g AndroidManifest.xml
+sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g src/com/smssync/portal/one/SmsSyncPortal.java
+sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g gen/com/smssync/portal/one/BuildConfig.java
+sed -i -e s/com.smssync.portal.one/com.smssync.portal.$version/g gen/com/smssync/portal/one/R.java
