3acdce4bb9c0be5074e6aba7dc15fbe43aca66c7
Vaneyck <vaneycksemeotoki@gmail.com>
2013-08-01 12:59:05 +0300

    Refactor sed

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 function buildApp {
-	local version=$1
+	local version="$1"
 
 	echo "# $version # :Changing package name on AndroidManifest.xml"
-	sed -i -e s/com.smssync.portal.million/com.smssync.portal.$version/g AndroidManifest.xml
+	sed -i -e s/million/$version/g AndroidManifest.xml
 
 	echo "# $version # Changing the package name on SmsSyncPortal.java"
-	sed -i -e s/com.smssync.portal.million/com.smssync.portal.$version/g src/com/smssync/portal/million/SmsSyncPortal.java
+	sed -i -e s/million/$version/g src/com/smssync/portal/million/SmsSyncPortal.java
 
 	echo "# $version # Making proper directory"
 	mkdir src/com/smssync/portal/$version
@@ -25,6 +25,6 @@ for verison in one
 do
 	buildApp $version
 	cp bin/SmsSyncPortal-debug.apk ~/Desktop/SmsSyncPortal-$version-debug.apk
-	git reset --hard && git clean -df
+	#git reset --hard && git clean -df
 done
 
